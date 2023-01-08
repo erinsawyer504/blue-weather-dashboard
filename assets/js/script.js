f0973cdf7707ff45e7dc6756d7adb745487314f1
erinsawyer504 <erinsawyer504@gmail.com>
2023-01-08 16:58:17 -0500

    added beginning variables and TODO lists

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let APIKey = "0a1a4289543c160384533f68bb935e5d";
+let city;
+
+//TODO: add a search for a city
+//TODO: add in buttons for previous cities searched
+//TODO: show city searched with current date, weather icon, temp (deg F), wind (MPH), humdity(%)
+//TODO: show 5 Day Forcast for city searched
+//TODO: add local storage
