d89dec5167c892f98e297a5e8ef5e2aff71ef2e3
erinsawyer504 <erinsawyer504@gmail.com>
2023-01-10 14:26:47 -0500

    added for loop for weather forcasting

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,14 @@ let windEl = document.querySelector('#wind');
 let humidityEl = document.querySelector('#humidity');
 let btnEl = document.querySelector('.btn');
 
+//Selecting 5 day forcast selectors
+// let day1El = document.querySelector('#day1');
+// let day2El = document.querySelector('#day2');
+// let day3El = document.querySelector('#day3');
+// let day4El = document.querySelector('#day4');
+// let day5El = document.querySelector('#day5');
 
-
+//API key 
 let APIKey = "447b3007c14c6a40015847c686fd487a";
 
 
@@ -28,12 +34,14 @@ let formInputSubmit = function (event) {
     let cityInput = cityInputEl.value.trim();
     if(cityInput){
         getCity(cityInput);
+        getForcast(cityInput);
         //adding searched cities to list
         let ul = document.getElementsByTagName("ul")[0];
         let li = document.createElement("li");
         li.textContent = city.value;
         ul.prepend(li);
         cityInputEl.value = '';
+        // currentDateEl.value='';
     } else {
         alert('Please enter a city name');
     }
@@ -74,29 +82,42 @@ let getCity = function(city) {
 
 // Get date, icon, temp, wind, humidity
 // date - dt_txt (day 1 is next day)
-// icon - weather.icon
-// temp -temp day or main temp i dont know
-// wind - wind speed
-// humidity - main humidity
 
-// let getForcast = function(city) {
-// let forcastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`;
-//     fetch(forcastURL)
-//     .then(function(response) {
-//         console.log(response);
-//         response.json().then(function(data) {
-//             console.log(data);
-//             // let ficn = data.weather[0].icon;
-//             // console.log(ficn)                
+let getForcast = function(city) {
+let forcastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`;
+    fetch(forcastURL)
+    .then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        for (let i=0; i<5; i++){
+            document.getElementById("day" +(i+1) +"Date").innerHTML = "Date: " + data.list[i].dt_txt;
+            document.getElementById("day" +(i+1) +"Icon").innerHTML = `<img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png"/>`;
+            document.getElementById("day" +(i+1) +"Temp").innerHTML = "Temp: " + data.list[i].main.temp + "°F";
+            document.getElementById("day" +(i+1) +"Wind").innerHTML = "Wind: " + data.list[i].wind.speed + " MPH";
+            document.getElementById("day" +(i+1) +"Humidity").innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
+            let fDate = data['list'][i]['dt_txt']
+                console.log(fDate)
+            let fIcn = data['list'][i]['weather'][0]['icon']
+                console.log(fIcn)                
+            let fLocalTemp = data['list'][i]['main']['temp']
+                console.log(fLocalTemp)
+            let fWindSpeed = data['list'][i]['wind']['speed']
+                console.log(fWindSpeed)
+            let fHumid = data['list'][i]['main']['humidity']
+                console.log(fHumid)
             
-//             let flocalTemp = data['list'][0]['main']['temp']
-//             console.log(flocalTemp)
-//             // let fwindSpeed = data['wind']['speed']
-//             // let fhumid = data['main']['humidity']
-
-//         })
-//     })
-// }
+            // day1El.innerHTML=`<p>${fDate}</p> 
+            // <img src="https://openweathermap.org/img/wn/${fIcn}@2x.png"/> 
+            // <p>Temp: ${fLocalTemp}°F</p>
+            // <p>Wind: ${fWindSpeed} MPH</p>
+            // <p>Humidity: ${fHumid}%</p>`
+            // day3El.innerHTML = `Temp: ${fLocalTemp}°F`
+            // day4El.innerHTML = `Wind: ${fWindSpeed} MPH`
+            // day5El.innerHTML = `Humidity: ${fHumid}%`
+        }
+        })
+    })
+}
 
 cityFormEl.addEventListener('submit', formInputSubmit);
-
